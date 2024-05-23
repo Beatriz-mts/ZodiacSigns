61376d9a8d999b8aff9346f71e48ff98b714c121
Beatriz-mts <beatrizmts@yahoo.com.br>
2024-05-23 13:39:31 +0200

    Added views, templates and urls for each zodiac sign list.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.zodiac_signs_list, name='zodiac_signs_list'),
+    path('', views.zodiac_sign_list, name='zodiac_sign_list'),
+    path('zodiac-signs/', views.zodiac_sign_list, name='zodiac_sign_list'),
+    path('aries/', views.aries_list, name='aries_list'),
+    path('taurus/', views.taurus_list, name='taurus_list'),
+    path('gemini/', views.gemini_list, name='gemini_list'),
+    path('cancer/', views.cancer_list, name='cancer_list'),
+    path('leo/', views.leo_list, name='leo_list'),
+    path('virgo/', views.virgo_list, name='virgo_list'),
+    path('libra/', views.libra_list, name='libra_list'),
+    path('scorpio/', views.scorpio_list, name='scorpio_list'),
+    path('sagittarius/', views.sagittarius_list, name='sagittarius_list'),
+    path('capricorn/', views.capricorn_list, name='capricorn_list'),
+    path('aquarius/', views.aquarius_list, name='aquarius_list'),
+    path('pisces/', views.pisces_list, name='pisces_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,81 @@
 from django.shortcuts import render
+from .models import ZodiacSign, FamousPerson
 
 
-def zodiac_signs_list(request):
-    return render(request, 'blog/zodiac_signs_list.html', {})
+def zodiac_sign_list(request):
+    # Query the ZodiacSign model and order by the first letter of the name
+    zodiac_signs = ZodiacSign.objects.all().order_by('name')
+    return render(request, 'blog/zodiac_sign_list.html', {'zodiac_signs': zodiac_signs})
+
+
+def aries_list(request):
+    # Query the FamousPerson model and order by the ones that have Aries as solar zodiac sign by the sign_id
+    signs_aries = FamousPerson.objects.filter(zodiac_sign=1)
+    return render(request, 'blog/aries_list.html', {'signs_aries': signs_aries})
+
+
+def taurus_list(request):
+    # Query the FamousPerson model and order by the ones that have Taurus as solar zodiac sign by the sign_id
+    signs_taurus = FamousPerson.objects.filter(zodiac_sign=2)
+    return render(request, 'blog/taurus_list.html', {'signs_taurus': signs_taurus})
+
+
+def gemini_list(request):
+    # Query the FamousPerson model and order by the ones that have Gemini as solar zodiac sign by the sign_id
+    signs_gemini = FamousPerson.objects.filter(zodiac_sign=3)
+    return render(request, 'blog/gemini_list.html', {'signs_gemini': signs_gemini})
+
+
+def cancer_list(request):
+    # Query the FamousPerson model and order by the ones that have Cancer as solar zodiac sign by the sign_id
+    signs_cancer = FamousPerson.objects.filter(zodiac_sign=4)
+    return render(request, 'blog/cancer_list.html', {'signs_cancer': signs_cancer})
+
+
+def leo_list(request):
+    # Query the FamousPerson model and order by the ones that have Leo as solar zodiac sign by the sign_id
+    signs_leo = FamousPerson.objects.filter(zodiac_sign=5)
+    return render(request, 'blog/leo_list.html', {'signs_leo': signs_leo})
+
+
+def virgo_list(request):
+    # Query the FamousPerson model and order by the ones that have Virgo as solar zodiac sign by the sign_id
+    signs_virgo = FamousPerson.objects.filter(zodiac_sign=6)
+    return render(request, 'blog/virgo_list.html', {'signs_virgo': signs_virgo})
+
+
+def libra_list(request):
+    # Query the FamousPerson model and order by the ones that have Libra as solar zodiac sign by the sign_id
+    signs_libra = FamousPerson.objects.filter(zodiac_sign=7)
+    return render(request, 'blog/libra_list.html', {'signs_libra': signs_libra})
+
+
+def scorpio_list(request):
+    # Query the FamousPerson model and order by the ones that have Scorpio as solar zodiac sign by the sign_id
+    signs_scorpio = FamousPerson.objects.filter(zodiac_sign=8)
+    return render(request, 'blog/scorpio_list.html', {'signs_scorpio': signs_scorpio})
+
+
+def sagittarius_list(request):
+    # Query the FamousPerson model and order by the ones that have Sagittarius as solar zodiac sign by the sign_id
+    signs_sagittarius = FamousPerson.objects.filter(zodiac_sign=9)
+    return render(request, 'blog/sagittarius_list.html', {'signs_sagittarius': signs_sagittarius})
+
+
+def capricorn_list(request):
+    # Query the FamousPerson model and order by the ones that have Capricorn as solar zodiac sign by the sign_id
+    signs_capricorn = FamousPerson.objects.filter(zodiac_sign=10)
+    return render(request, 'blog/capricorn_list.html', {'signs_capricorn': signs_capricorn})
+
+
+def aquarius_list(request):
+    # Query the FamousPerson model and order by the ones that have Aquarius as solar zodiac sign by the sign_id
+    signs_aquarius = FamousPerson.objects.filter(zodiac_sign=11)
+    return render(request, 'blog/aquarius_list.html', {'signs_aquarius': signs_aquarius})
+
+
+def pisces_list(request):
+    # Query the FamousPerson model and order by the ones that have Pisces as solar zodiac sign by the sign_id
+    signs_pisces = FamousPerson.objects.filter(zodiac_sign=12)
+    return render(request, 'blog/pisces_list.html', {'signs_pisces': signs_pisces})
 
